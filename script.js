bd42411ec0fe4ab5f9d644aaa6a38c1e938995b0
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-08-06 16:48:37 +0330

    Create logic for adding books and displaying them

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+const section = document.querySelector("section")
 const myLibrary = [];
+
 
 function Book(title, author, pages, isRead) {
     this.title = title;
@@ -8,9 +10,24 @@ function Book(title, author, pages, isRead) {
 }
 
 function addBookToLibrary() {
+    let title = prompt("title?")
+    let author = prompt("author?")
+    let pages = prompt("How many pages?")
+    let status = prompt("Have you read it? y/n")
+    let statusBoolean = (status === "y") ? true : false
+    if (title === "" || author === "" || pages === "" || status === "") return
+    if (!title || !author || !pages || !status) return
+    let newBook = new Book(title, author, pages, statusBoolean)
+    myLibrary.push(newBook)
 
+    myLibrary.forEach((book) => {
+        const bookCard = document.createElement("div")
+        const bookTitle = document.createElement("h2")
+        bookCard.classList.toggle("card")
+        bookTitle.textContent = book.title
+        section.appendChild(bookCard)
+        bookCard.appendChild(bookTitle)
+    })
 }
 
-const HP = new Book("Harry Potter", "JK Rowling", "210", true)
-const hobbit = new Book("The Hobbit", "Tolkein", "423", false)
-const influence = new Book("Influence", "Robert B. Cialdini", "314", true)
+// addBookToLibrary()
