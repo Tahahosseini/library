efbb3cf0efdc00fe41f76eabcb7e665bd502411a
Taha Lucid <98825024+Tahahosseini@users.noreply.github.com>
2024-08-08 02:56:38 +0330

    Optimize for readability

--- script.js
+++ script.js
@@ -9,33 +9,13 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-function addBookToLibrary() {
-    const title = prompt("title?")
-    const author = prompt("author?")
-    const pages = prompt("How many pages?")
-    const status = prompt("Have you read it? y/n")
-    const statusBoolean = (status === "y") ? true : false
-    if (!title || !author || !pages || !status) return
-    const newBook = new Book(title, author, pages, statusBoolean)
-    myLibrary.push(newBook)
-    updateCards()
-}
-
-let bookCard;
-let allBookCards;
 function updateCards() {
-    if (bookCard) {
-        allBookCards.forEach((card) => {
-            booksGrid.removeChild(card)
-        })
-    }
+    booksGrid.textContent = ""
 
     myLibrary.forEach((book, index) => {
-        bookCard = document.createElement("div")
+        const bookCard = document.createElement("div")
         bookCard.className = "card"
         booksGrid.appendChild(bookCard)
-
-        allBookCards = document.querySelectorAll(".card")
 
         const bookTitle = document.createElement("h2")
         bookTitle.textContent = book.title
@@ -54,12 +34,25 @@ function updateCards() {
         removeButton.setAttribute("role", "button")
         removeButton.textContent = "Remove Book"
         bookCard.appendChild(removeButton)
-        removeButton.addEventListener("click", (e) => {
-            myLibrary.splice(index, 1);
-            e.target.parentNode.remove()
-            allBookCards = document.querySelectorAll(".card")
-        })
+        removeButton.addEventListener("click", () => removeFromLibrary(index))
     })
+}
+
+function addBookToLibrary() {
+    const title = prompt("title?")
+    const author = prompt("author?")
+    const pages = prompt("How many pages?")
+    const status = prompt("Have you read it? y/n")
+    const statusBoolean = (status === "y") ? true : false
+    if (!title || !author || !pages || !status) return
+    const newBook = new Book(title, author, pages, statusBoolean)
+    myLibrary.push(newBook)
+    updateCards()
+}
+
+function removeFromLibrary(index) {
+    myLibrary.splice(index, 1);
+    updateCards()
 }
 
 function toggleStatusButton(index) {
